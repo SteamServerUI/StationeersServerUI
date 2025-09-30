50b007f0f12be54d79d70c22393901c77940fc45
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-09-30 13:30:57 +0200

    reflect runState in http code as well

--- src/web/monitoring.go
+++ src/web/monitoring.go
@@ -13,6 +13,13 @@ func HandleMonitorStatus(w http.ResponseWriter, r *http.Request) {
 		"isRunning": runState,
 	}
 	w.Header().Set("Content-Type", "application/json")
+
+	if !runState {
+		w.WriteHeader(http.StatusServiceUnavailable) // 503
+	} else {
+		w.WriteHeader(http.StatusOK) // 200
+	}
+
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, "Failed to respond with Game Server status", http.StatusInternalServerError)
 		return
